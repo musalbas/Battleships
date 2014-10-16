7c9542a95ef5fff7be2a0bd4fe047b88cae3a19c
andreiju <ajuganaru@outlook.com>
2014-10-16 02:23:17 +0100

    new model classes
    combined and improved the 2 model classes
    -method names will stay the same from now on-

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -1,99 +1,107 @@
-public class Board
-{
-    private final int BOARD_DIMENSION = 10;
-    private Square[][] squares;
-    private Ship[] ships;
-    
-    //constructs a board with no ships
-    public Board()
-    {
-        squares = new Square[BOARD_DIMENSION][BOARD_DIMENSION];
-        
-        //populates the squares array
-        for(int i = 0; i < BOARD_DIMENSION; i++)
-        {
-            for(int j = 0; j < BOARD_DIMENSION; j++)
-            {
-                squares[i][j] = new Square();
-            }
-        }
-        
-        ships = new Ship[5];
-    }
-    
-    public Square getSquare(int x, int y)
-    {
-        return squares[x][y];
-    }
-    
-    public void setHit(Square s)
-    {
-        s.setHit(true);
-        
-        if(s.getShip() != null) //if s has a ship on it
-        {
-            s.getShip().decreaseHealth();
-        }
-    }
-    
-    //places a ship on the board with the parameters x and y being
-    //the co-ordinates of the head of the ship
-    public void addShip(Ship ship, int x, int y)
-    {
-        //finds the first empty cell in ships array and stores the ship in it
-        for(int i = 0; i < ships.length; i++)
-        {
-            if(ships[i] == null)
-            {
-                ships[i] = ship;
-                break;
-            }
-        }
-        
-        //puts ship on squares
-        for(int i = 0; i < ship.getLength(); i++)
-        {
-            if(ship.getOrientation() == Ship.HORIZONTAL)
-            {
-                squares[x][y + i].setShip(ship);
-            }
-            else if(ship.getOrientation() == Ship.VERTICAL)
-            {
-                squares[x + i][y].setShip(ship);
-            }
-        }
-    }
-    
-    public boolean gameOver()
-    {
-        for(int i = 0; i < ships.length; i++)
-        {
-            if(!ships[i].isSunk())
-            {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    //for testing, prints "S" if square is ship, otherwise "0" for normal square
-    public void printBoard()
-    {
-        for(int i = 0; i < 10; i++)
-        {
-            for(int j = 0; j < 10; j++)
-            {
-                if(squares[i][j].isShip())
-                {
-                    System.out.print("S");
-                }
-                else
-                {
-                    System.out.print("0");
-                }
-            }
-            System.out.println();
-        }
-    }
+import java.util.ArrayList;
+
+public class Board {
+	private final int BOARD_DIMENSION = 10;
+	private Square[][] squares;
+	private ArrayList<Ship> ships;
+	private boolean ownBoard;
+
+	public Board(boolean ownBoard) {
+		this.ownBoard = ownBoard;
+		squares = new Square[BOARD_DIMENSION][BOARD_DIMENSION];
+
+		// populates the squares array
+		for (int i = 0; i < BOARD_DIMENSION; i++) {
+			for (int j = 0; j < BOARD_DIMENSION; j++) {
+				squares[i][j] = new Square(i, j, ownBoard);
+			}
+		}
+
+		ships = new ArrayList<Ship>();
+		ships.add(new Ship(Ship.Type.AIRCRAFT_CARRIER));
+		ships.add(new Ship(Ship.Type.BATTLESHIP));
+		ships.add(new Ship(Ship.Type.DESTROYER));
+		ships.add(new Ship(Ship.Type.PATROL_BOAT));
+		ships.add(new Ship(Ship.Type.SUBMARINE));
+	}
+
+	public boolean isOwnBoard() {
+		return (ownBoard);
+	}
+
+	public Square getSquare(int x, int y) {
+		return squares[x][y];
+	}
+
+	public boolean placeShip(Ship ship, int x, int y) {
+
+		int end = (ship.isVertical()) ? x + ship.getLength() - 1 : y
+				+ ship.getLength() - 1;
+		if (x < 0 || y < 0 || end >= BOARD_DIMENSION) {
+			return false; // doesn't fit on board
+		}
+
+		// puts ship on squares
+		for (int i = 0; i < ship.getLength(); i++) {
+			if (ship.isVertical()) {
+				squares[x + i][y].setShip(ship);
+				ship.setSquare(squares[x + i][y]);
+			} else if (!ship.isVertical()) {
+				squares[x][y + i].setShip(ship);
+				ship.setSquare(squares[x][y + i]);
+			}
+		}
+
+		return true;
+
+	}
+
+	public void pickUpShip(Ship ship) {
+		for (Square s : ship.getSquares()) {
+			s.setShip(null);
+		}
+		ship.clearSquares();
+	}
+
+	public void setHit(Square s) {
+		s.setHit(true);
+	}
+
+	public boolean gameOver() {
+		for (Ship ship : ships) {
+			if (!ship.isSunk())
+				return false;
+		}
+		return true;
+	}
+
+	public void printBoard() {
+		for (int i = 0; i < BOARD_DIMENSION; ++i) {
+			for (int j = 0; j < BOARD_DIMENSION; ++j) {
+				Square s = squares[i][j];
+				Ship ship = s.getShip();
+				char c = '-';
+				if (ship != null) {
+					switch (ship.getType()) {
+					case AIRCRAFT_CARRIER:
+						c = 'A';
+						break;
+					case BATTLESHIP:
+						c = 'B';
+						break;
+					case SUBMARINE:
+						c = 'S';
+						break;
+					case DESTROYER:
+						c = 'D';
+						break;
+					case PATROL_BOAT:
+						c = 'p';
+					}
+				}
+				System.out.print(c + " ");
+			}
+			System.out.println();
+		}
+	}
 }
